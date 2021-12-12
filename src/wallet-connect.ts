a9db8176041879e273679823e9cc1d388195f4b2
Loïc Vincent-Genod <37535451+VGLoic@users.noreply.github.com>
2021-12-12 17:11:01 +0100

    feat: add wallet connect (#5)

---
+++ src/wallet-connect.ts
@@ -0,0 +1,15 @@
+import { Provider } from "./provider";
+
+export class WalletConnectProvider extends Provider {
+
+  async disconnect() {
+      return;
+  }
+
+  async enable(): Promise<string[]> {
+      return this.request({
+          method: "eth_accounts",
+          params: []
+      }) as Promise<string[]>;
+  }
+}
