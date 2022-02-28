77bd2885dd1e5c0ee95f433411c438f5658f2079
VGLoic <vincent.genod.loic@gmail.com>
2022-02-28 10:17:09 +0100

    feat: add comments, fix code

--- src/testing-utils/ens-utils/index.ts
+++ src/testing-utils/ens-utils/index.ts
@@ -25,6 +25,14 @@ export class EnsUtils {
     );
   }
 
+  /**
+   * Mock the resolver address of a name
+   * @param name ENS name
+   * @param resolverAddress Resolver address, fallback to the main net public resolver
+   * @example ```ts
+   * ens.mockResolver("blabla.eth");
+   * ```
+   */
   public mockResolver(
     name?: string,
     resolverAddress = PUBLIC_RESOLVER_ADDRESS
@@ -43,6 +51,12 @@ export class EnsUtils {
     return this;
   }
 
+  /**
+   * Mock all addresses to resolve to empty names and names to zero address
+   * @example ```ts
+   * ens.mockAllToEmpty();
+   * ```
+   */
   public mockAllToEmpty() {
     this.mockResolver();
     this.publicResolverUtils.mockCall("name", [""], undefined, {
@@ -56,18 +70,54 @@ export class EnsUtils {
     );
   }
 
-  public mockName(address: string, name: string) {
+  /**
+   * Mock the resolution of a name to an address
+   * @param name ENS name
+   * @param address Address
+   * @example ```ts
+   * ens.mockResolve("blabla.eth", "0x123...");
+   * ```
+   */
+  public mockResolve(name: string, address: string) {
     this.mockResolver(name);
-    const callValues = name
-      ? {
-          callValues: [ethers.utils.namehash(name)],
-        }
-      : undefined;
-    this.publicResolverUtils.mockCall("name", [name], callValues, {
-      persistent: true,
-    });
+    const callValues = {
+      callValues: [ethers.utils.namehash(name)],
+    };
     this.publicResolverUtils.mockCall("addr(bytes32)", [address], callValues, {
       persistent: true,
     });
+  }
+
+  /**
+   * Mock the reverse resolution of an address to a name
+   * @param address Address
+   * @param name ENS name
+   * @example ```ts
+   * ens.mockReverseResolve("0x123...", "blabla.eth");
+   * ```
+   */
+  public mockReverseResolve(address: string, name: string) {
+    const reverseName = address.substring(2).toLowerCase() + ".addr.reverse";
+
+    this.mockResolver(reverseName);
+    const callValues = {
+      callValues: [ethers.utils.namehash(reverseName)],
+    };
+    this.publicResolverUtils.mockCall("name", [name], callValues, {
+      persistent: true,
+    });
+  }
+
+  /**
+   * Mock a name to resolve to an address and the address to reverse resolve to the name
+   * @param address Address
+   * @param name ENS name
+   * @example ```ts
+   * ens.mockName("0x123...", "blabla.eth");
+   * ```
+   */
+  public mockEnsName(address: string, name: string) {
+    this.mockResolve(name, address);
+    this.mockReverseResolve(address, name);
   }
 }
