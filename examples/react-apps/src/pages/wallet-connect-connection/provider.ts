98966dd4d8fd971d42873702e37d828b54651652
VGLoic <vincent.genod.loic@gmail.com>
2022-03-03 16:22:27 +0100

    feat: default infuraID to the one of the doc

--- examples/react-apps/src/pages/wallet-connect-connection/provider.ts
+++ examples/react-apps/src/pages/wallet-connect-connection/provider.ts
@@ -1,7 +1,7 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const provider = new WalletConnectProvider({
-    infuraId: process.env.REACT_APP_INFURA_KEY || "",
+    infuraId: process.env.REACT_APP_INFURA_KEY || "27e484dcd9e3efcfd25a83a78777cdf1",
 });
 
 export function getWalletConnectProvider() {
