34ed017a2fc145d53af7914be01cb8fe12896da7
VGLoic <vincent.genod.loic@gmail.com>
2022-03-03 16:12:14 +0100

    feat: fallback infura ID to empty string

--- examples/react-apps/src/pages/wallet-connect-connection/provider.ts
+++ examples/react-apps/src/pages/wallet-connect-connection/provider.ts
@@ -1,7 +1,7 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const provider = new WalletConnectProvider({
-    infuraId: process.env.REACT_APP_INFURA_KEY,
+    infuraId: process.env.REACT_APP_INFURA_KEY || "",
 });
 
 export function getWalletConnectProvider() {
