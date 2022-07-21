6a20ac8b40847fc2ea47796473854cbb307d61d1
VGLoic <vincent.genod.loic@gmail.com>
2022-07-21 08:32:24 +0200

    feat: remove unused imports

--- examples/react-apps/src/pages/web3-react/__test__/web3-react-page.test.tsx
+++ examples/react-apps/src/pages/web3-react/__test__/web3-react-page.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { generateTestingUtils } from "eth-testing";
 import Web3ReactPage from "..";
