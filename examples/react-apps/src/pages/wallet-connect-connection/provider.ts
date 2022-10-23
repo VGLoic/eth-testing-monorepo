beb35a708a836092e05effdddb79af29d80714a1
VGLoic <vincent.genod.loic@gmail.com>
2022-10-23 16:58:35 +0200

    feat: linter

--- examples/react-apps/src/pages/wallet-connect-connection/provider.ts
+++ examples/react-apps/src/pages/wallet-connect-connection/provider.ts
@@ -1,9 +1,10 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const provider = new WalletConnectProvider({
-    infuraId: process.env.REACT_APP_INFURA_KEY || "27e484dcd9e3efcfd25a83a78777cdf1",
+  infuraId:
+    process.env.REACT_APP_INFURA_KEY || "27e484dcd9e3efcfd25a83a78777cdf1",
 });
 
 export function getWalletConnectProvider() {
-    return provider;
+  return provider;
 }
